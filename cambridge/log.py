0dc535ae892664a8a532d813271f323a9e12d12a
finpluto <finpluto@hotmail.com>
2022-08-23 11:11:07 +0800

    fix: remove static logging init when importing

--- cambridge/log.py
+++ cambridge/log.py
@@ -8,15 +8,15 @@ import logging
 # data = Path.home() / ".local" / "share" / "cambridge"
 # data.mkdir(parents=True, exist_ok=True)
 
-logging.basicConfig(
-    # filename=str(data / "dict.log"),
-    # filemode="a",
-    # format="%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s",
-    format="%(asctime)s %(levelname)s %(filename)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
-)
+# logging.basicConfig(
+#    # filename=str(data / "dict.log"),
+#    # filemode="a",
+#    # format="%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s",
+#    format="%(asctime)s %(levelname)s %(filename)s %(message)s",
+#    datefmt="%Y-%m-%d %H:%M:%S",
+#    level=logging.INFO,
+# )
 
 logger = logging.getLogger(
-    __package__
+    __name__
 )  # if imported, all modules will have the same logger called by name of __package__'s value'
